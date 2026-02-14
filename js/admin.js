f02d0d5f74e96ea7705cbf8657eeda024d581060
a-cuita <bio.browning@gmail.com>
2026-02-13 18:55:03 -0600

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -179,6 +179,7 @@ function showAdminScreen() {
     loadAdminSiteStatus();
     loadAdminBanners();
     loadAdminPendingEvents();
+    loadAdminUserOverrides();
 }
 
 function toggleAdminSection(headerEl) {
